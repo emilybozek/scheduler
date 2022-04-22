f2a401d76f14f343cdb81f247c28dc594fdc90c9
Emily Bozek <emily.bozek@gmail.com>
2022-04-21 22:20:42 -0700

    add Appointment views and form stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,12 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index.jsx';
 import Header from 'components/Appointment/Header.jsx';
+import Confirm from 'components/Appointment/Confirm.jsx';
+import Empty from 'components/Appointment/Empty.jsx';
+import Show from 'components/Appointment/Show.jsx';
+import Status from 'components/Appointment/Status.jsx';
+import Error from 'components/Appointment/Error.jsx';
+import Form from 'components/Appointment/Form.jsx';
 
 storiesOf('Button', module)
   .addParameters({
@@ -128,11 +134,65 @@ storiesOf('InterviewerList', module)
     />
   ));
 
-  storiesOf("Appointment", module)
+storiesOf('Appointment', module)
   .addParameters({
-    backgrounds: [{ name: "white", value: "#fff", default: true }]
+    backgrounds: [{ name: 'white', value: '#fff', default: true }],
   })
-  .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add('Appointment', () => <Appointment />)
+  .add('Appointment with Time', () => <Appointment time="12pm" />)
+  .add('Header', () => <Header time="12pm" />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Status', () => <Status message="Deleting" />)
+  .add('Error', () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action('onClose')}
+    />
+  ))
+  .add('Edit Form', () => (
+    <Form
+      student="Bobby Bob"
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Create Form', () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Appointment Empty', () => (
+    <>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </>
+  ))
+  .add('Appointment Booked', () => (
+    <>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: 'Lydia Miller-Jones', interviewer }}
+      />
+      <Appointment time="5pm" />
+    </>
+  ));
