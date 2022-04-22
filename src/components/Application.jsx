fb6698a1a037e47af80cd3c0b19f7bd80feef6d6
Emily Bozek <emily.bozek@gmail.com>
2022-04-21 20:18:20 -0700

    alter file type

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,24 +3,64 @@ import React, { useState } from 'react';
 import 'components/Application.scss';
 import DayList from 'components/DayList';
 
+const days = [
+  {
+    id: 1,
+    name: 'Monday',
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: 'Tuesday',
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: 'Wednesday',
+    spots: 0,
+  },
+];
+
+const appointments = {
+  1: {
+    id: 1,
+    time: '12pm',
+  },
+  2: {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 3,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: '2pm',
+  },
+  4: {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Archie Andrews',
+      interviewer: {
+        id: 4,
+        name: 'Cohana Roy',
+        avatar: 'https://i.imgur.com/FK8V841.jpg',
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: '4pm',
+  },
+};
+
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: 'Monday',
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: 'Tuesday',
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: 'Wednesday',
-      spots: 0,
-    },
-  ];
   const [day, setDay] = useState('Monday');
 
   return (
@@ -33,7 +73,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={'Monday'} setDay={setDay} />
+          <DayList days={days} day={'Monday'} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
