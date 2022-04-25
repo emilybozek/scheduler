8036aacea3569d247a545c73bf011a47f622d74c
Emily Bozek <emily.bozek@gmail.com>
2022-04-24 20:07:22 -0700

    alter schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,55 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Application.scss';
 import DayList from '../components/DayList';
 import Appointment from '../components/Appointment';
 import axios from 'axios';
+import { getAppointmentsForDay } from '../helpers/selectors';
 
-const appointments = {
-  1: {
-    id: 1,
-    time: '12pm',
-  },
-  2: {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 3,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: '2pm',
-  },
-  4: {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Archie Andrews',
-      interviewer: {
-        id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg',
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: '4pm',
-  },
-};
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
   const [days, setDays] = useState([]);
+  // eslint-disable-next-line
   const [appointments, setAppointments] = useState({});
+  // eslint-disable-next-line
   const [interviewers, setInterviewers] = useState({});
+  const dailyAppointments = getAppointmentsForDay(day);
 
-  const schedule = props.appointments.map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />;
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
   });
 
   useEffect(() => {
@@ -58,9 +33,9 @@ export default function Application(props) {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers'),
     ]).then((all) => {
-      setDays(all[0]);
-      setAppointments(all[1]);
-      setInterviewers(all[2]);
+      setDays(all[0].data);
+      setAppointments(all[1].data);
+      setInterviewers(all[2].data);
     });
   }, []);
 
@@ -74,7 +49,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList value={day} onChange={setDay} />
+          <DayList value={day} days={days} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
