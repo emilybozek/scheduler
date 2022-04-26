7db8e880339c5dc6dcb5d172504efdd91490e2b1
Emily Bozek <emily.bozek@gmail.com>
2022-04-26 14:52:13 -0700

    build

---
+++ storybook-static/precache-manifest.dd9a089025bebe4c84ec09fe8acdfc50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421eca57c34c9fc16f45fa98e89d0b42",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4fa82634dff17ff6cfa5.bundle.js"
+  },
+  {
+    "url": "runtime~main.4fa82634dff17ff6cfa5.bundle.js"
+  },
+  {
+    "revision": "3062d8aa02534c6c4899",
+    "url": "static/css/main.550ec172.chunk.css"
+  },
+  {
+    "revision": "33f19cc707773b51c64e",
+    "url": "static/css/vendors~main.9aaa97ae.chunk.css"
+  },
+  {
+    "url": "vendors~main.4fa82634dff17ff6cfa5.bundle.js"
+  },
+  {
+    "url": "vendors~main.4fa82634dff17ff6cfa5.bundle.js.LICENSE.txt"
+  }
+]);
