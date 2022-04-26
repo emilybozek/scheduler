28716bc0b5e84ca60dd51d36914f1dc6af8a86b0
Emily Bozek <emily.bozek@gmail.com>
2022-04-26 14:50:38 -0700

    alter file paths

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
-import 'components/InterviewerList.scss';
+import '../components/InterviewerList.scss';
 
 export default function InterviewerList(props) {
+  console.log('INTERVIEWERLIST PROPS', props);
   const interviewerArray = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
