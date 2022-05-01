41ae844fe06af3bbe40ce20ed2a8897f0e22ca8a
Emily Bozek <emily.bozek@gmail.com>
2022-05-01 15:08:22 -0700

    import cleanup from testing library

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
 
-import Appointment from "components/Appointment/index";
+import { render, cleanup } from '@testing-library/react';
 
-describe("Appointment", () => {
-  it("renders without crashing", () => {
+import Appointment from 'components/Appointment';
+
+afterEach(cleanup);
+
+describe('Appointment', () => {
+  it('renders without crashing', () => {
     render(<Appointment />);
   });
 });
